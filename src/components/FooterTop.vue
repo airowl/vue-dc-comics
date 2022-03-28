93c800816715a81c04ae9f42f98fb0ec0be517a5
rowl <airowl.gasga@gmail.com>
2022-03-28 19:33:18 +0200

    inserito nel footer top array di oggetti per i links

--- src/components/FooterTop.vue
+++ src/components/FooterTop.vue
@@ -5,49 +5,31 @@
                 <div class="col">
                     <h4>Dc Comics</h4>
                     <ul>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
+                        <li v-for="(link, index) in dcComicsLinks" :key="index">
+                            <a :href="link.url">{{link.text}}</a>
+                        </li>
                     </ul>
                     <h4>Shop</h4>
                     <ul>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
+                        <li v-for="(link, index) in shopLinks" :key="index">
+                            <a :href="link.url">{{link.text}}</a>
+                        </li>
                     </ul>
                 </div>
                 <div class="col">
                     <h4>Dc</h4>
                     <ul>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
+                        <li v-for="(link, index) in dcLinks" :key="index">
+                            <a :href="link.url">{{link.text}}</a>
+                        </li>
                     </ul>
                 </div>
                 <div class="col">
                     <h4>Sites</h4>
                     <ul>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
-                        <li><a href="#"></a>Item</li>
+                        <li v-for="(link, index) in sitesLinks" :key="index">
+                            <a :href="link.url">{{link.text}}</a>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -59,7 +41,158 @@
 
 <script>
 export default {
-    name: 'FooterTop'
+    name: 'FooterTop',
+    data: function(){
+        return {
+            dcComicsLinks: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Tv',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                }
+            ],
+            shopLinks: [
+                {
+                    text: 'Shop DC',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop DC Collectibles',
+                    url: '#',
+                    current: false,
+                }
+            ],
+            dcLinks: [
+                {
+                    text: 'Term of Use',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Privacy policy (New)',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Ad Choices',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Advertising',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Jobs',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Subscriptions',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Talent Workshop',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'CPSC Certificates',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Ratings',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop Help',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Contact Us',
+                    url: '#',
+                    current: false,
+                }
+            ],
+            sitesLinks: [
+                {
+                    text: 'DC',
+                    url: '#',
+                    current: false,
+                },{
+                    text: 'MAD Magazine',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'DC Kids',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'DC Universe',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'DC Power Visa',
+                    url: '#',
+                    current: false,
+                },
+            ]
+        }
+    }
 }
 </script>
 
@@ -96,7 +229,10 @@ export default {
 
                 li{
                     font-size: .6rem;
-                    color: #7f7f7f;
+
+                    a{
+                        color: #b4b4b4;
+                    }
                 }
             }
 
