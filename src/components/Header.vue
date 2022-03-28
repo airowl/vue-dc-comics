df5ee11b9d3f6556d1b1389122f5bc725ba29a3d
rowl <airowl.gasga@gmail.com>
2022-03-28 16:13:34 +0200

    creato scss variabili e aggiunto logo + links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,22 @@
 <template>
-    <div>
-
+    <div id="header">
+        <div class="main-logo">
+            <img src="../assets/img/dc-logo.png" alt="Logo DC">
+        </div>
+        <nav class="header-nav">
+            <ul>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Characters</a></li>
+            </ul>
+        </nav>
     </div>
 </template>
 
@@ -12,5 +28,30 @@ export default {
 
 <style lang="scss" scoped>
 @import "../assets/scss/style.scss";
+
+#header{
+    @include container;
+    @include d-flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid red;
+
+    nav.header-nav{
+        
+        ul{
+            @include d-flex;
+
+            li{
+                list-style-type: none;
+
+                a{
+                    text-decoration: none;
+                    color: $fontColor;
+                }
+            }
+        }
+    }
+
+}
 
 </style>
