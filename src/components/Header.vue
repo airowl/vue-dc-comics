099a3520a0cc941b1d0e4c54bded6244f56cf0e2
rowl <airowl.gasga@gmail.com>
2022-03-28 16:53:14 +0200

    completato l'header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,15 +6,15 @@
         <nav class="header-nav">
             <ul>
                 <li><a href="#">Characters</a></li>
-                <li><a href="#">Characters</a></li>
-                <li><a href="#">Characters</a></li>
-                <li><a href="#">Characters</a></li>
-                <li><a href="#">Characters</a></li>
-                <li><a href="#">Characters</a></li>
-                <li><a href="#">Characters</a></li>
-                <li><a href="#">Characters</a></li>
-                <li><a href="#">Characters</a></li>
-                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
             </ul>
         </nav>
     </div>
@@ -34,19 +34,36 @@ export default {
     @include d-flex;
     justify-content: space-between;
     align-items: center;
+    height: 15vh;
     border: 1px solid red;
 
     nav.header-nav{
         
+        height: 100%;
+
         ul{
+            height: 100%;
             @include d-flex;
+            justify-content: center;
 
             li{
                 list-style-type: none;
+                margin-left: 2rem;
+                height: 100%;
+                @include d-flex;
+                align-items: center;
+                border-bottom: 4px solid transparent;
+
+                &.active,
+                &:hover{
+                    border-bottom: 4px solid $secondaryColor;
+                }
 
                 a{
                     text-decoration: none;
                     color: $fontColor;
+                    text-transform: uppercase;
+                    font-size: .8rem;
                 }
             }
         }
